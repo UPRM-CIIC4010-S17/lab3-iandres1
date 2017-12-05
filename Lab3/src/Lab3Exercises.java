42eeea95e16a298d76d6571494685bc8d3330f49
Ismael Rios <ismael.rios2@upr.edu>
2017-12-05 15:07:49 -0500

    panel in the middle

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,11 +1,21 @@
 import javax.swing.JFrame;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
+		JFrame myFrame = new JFrame("Ismael");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+	    int width = (int) screenSize.getWidth();      // the screen width
+	    int height = (int) screenSize.getHeight();    // the screen height
+	    myFrame.setLocation((width/2) -100, (height/2) -100);
+		myFrame.setSize(200, 200);
+		
+		MyPanelClass myPanel = new MyPanelClass();
+		myFrame.getContentPane().add(myPanel);
+		
 		myFrame.setVisible(true);
 	}
 }
